b0119ff3198992b9735b01b35cedd8507b7ad0e5
AleksandrVavilov <vavilov3388@gmail.com>
2022-10-21 11:06:48 +0300

    Initial commit

---
+++ src/main/java/com/company/libraryhw/LibraryHwApplication.java
@@ -0,0 +1,13 @@
+package com.company.libraryhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryHwApplication.class, args);
+    }
+
+}
